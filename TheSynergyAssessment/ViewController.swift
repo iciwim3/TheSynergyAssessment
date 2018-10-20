6dc74057f2d043bdf41ce7bf1a48119ed9b61bcb
SKE <appybuildmore@gmail.com>
2018-10-19 19:05:10 -0700

    Initial Commit

---
+++ TheSynergyAssessment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheSynergyAssessment
+//
+//  Created by Sain-R Edwards on 10/19/18.
+//  Copyright © 2018 Swift Koding 4 Everyone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
