338a9963a74cc4ebe50edd66b98a3a5d1b3dd2f8
SKE <appybuildmore@gmail.com>
2018-10-19 21:08:23 -0700

    Created welcome screen for RP assessment app.

---
+++ TheSynergyAssessment/DesignableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DesignableViewController.swift
+//  TheSynergyAssessment
+//
+//  Created by Sain-R Edwards on 10/19/18.
+//  Copyright © 2018 Swift Koding 4 Everyone. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableViewController: UIViewController {
+    
+    @IBInspectable var lightStatusBar: Bool = false
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        get {
+            if lightStatusBar {
+                return UIStatusBarStyle.lightContent
+            } else {
+                return UIStatusBarStyle.default
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- TheSynergyAssessment/ViewController.swift
+++ TheSynergyAssessment/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
+@IBDesignable
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 
 
