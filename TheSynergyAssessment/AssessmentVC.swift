33d6c2150c2eab2feca5fef5a4a254303e6f7298
SKE <appybuildmore@gmail.com>
2018-10-27 22:00:07 -0700

    Updated tableview and view controller.

--- TheSynergyAssessment/AssessmentVC.swift
+++ TheSynergyAssessment/AssessmentVC.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class AssessmentVC: UIViewController, UITableViewDelegate, UITableViewDataSource, AssessmentProtocol {
     
-    
     var model = AssessmentModel()
     var statements = [Assessment]()
+    var statementIndex = 4
     var catalystIndex = 0
     var analystIndex = 0
     var stabilizerIndex = 0
@@ -24,9 +24,11 @@ class AssessmentVC: UIViewController, UITableViewDelegate, UITableViewDataSource
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Set self as delegate for model and call getStatements
         model.delegate = self
         model.getStatements()
-
+        
+        // Conform to table view protocols
         statementTableview.delegate = self
         statementTableview.dataSource = self
     }
@@ -39,13 +41,25 @@ class AssessmentVC: UIViewController, UITableViewDelegate, UITableViewDataSource
         return .lightContent
     }
     
+    // Tableview protocol methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        
+        guard statements[statementIndex].statementNumber != nil else {
+            return 0
+        }
+        
+        return statements[statementIndex].statementNumber!
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: STATEMENT_CELL, for: indexPath) as! StatementCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: STATEMENT_CELL, for: indexPath)
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // User has selected an answer
+        
     }
     
     // MARK: - AssessmentProtocol methods
