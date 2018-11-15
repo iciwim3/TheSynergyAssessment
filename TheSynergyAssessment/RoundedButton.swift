f539ddb47ece6233ab1e55b1628d28f33d7c90dc
SKE <appybuildmore@gmail.com>
2018-11-15 05:02:27 -0800

    Created an extension for the RoundedButton class for different colors to appear on the buttons in various control states.

--- TheSynergyAssessment/RoundedButton.swift
+++ TheSynergyAssessment/RoundedButton.swift
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+import ObjectiveC
+
+var defaultColorHandle: UInt8 = 0
+var disabledColorHandle: UInt8 = 0
+var selectedColorHandle: UInt8 = 0
 
 @IBDesignable
 class RoundedButton: UIButton {
@@ -17,4 +22,66 @@ class RoundedButton: UIButton {
         }
     }
 
+}
+
+extension RoundedButton {
+    
+    func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
+        self.setBackgroundImage(image(for: state), for: state)
+    }
+    
+    @IBInspectable var defaultColor: UIColor? {
+        get {
+            if let color = objc_getAssociatedObject(self, &defaultColorHandle) as? UIColor {
+                return color
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                self.setBackgroundColor(color, for: .selected)
+                objc_setAssociatedObject(self, &defaultColorHandle, color, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            } else {
+                self.setBackgroundImage(nil, for: .selected)
+                objc_setAssociatedObject(self, &defaultColorHandle, nil, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            }
+        }
+    }
+    
+    @IBInspectable var selectedColor: UIColor? {
+        get {
+            if let color = objc_getAssociatedObject(self, &selectedColorHandle) as? UIColor {
+                return color
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                self.setBackgroundColor(color, for: .selected)
+                objc_setAssociatedObject(self, &selectedColorHandle, color, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            } else {
+                self.setBackgroundImage(nil, for: .selected)
+                objc_setAssociatedObject(self, &selectedColorHandle, nil, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            }
+        }
+    }
+    
+    @IBInspectable var disabledColor: UIColor? {
+        get {
+            if let color = objc_getAssociatedObject(self, &disabledColorHandle) as? UIColor {
+                return color
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                self.setBackgroundColor(color, for: .disabled)
+                objc_setAssociatedObject(self, &disabledColorHandle, color, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            } else {
+                self.setBackgroundImage(nil, for: .disabled)
+                objc_setAssociatedObject(self, &disabledColorHandle, nil, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            }
+        }
+    }
+    
 }
