7e26a813c751d52d992dcaa1ded2b3d7d272cb0e
SKE <appybuildmore@gmail.com>
2018-11-15 19:45:32 -0800

    Updated Rounded Button class to change background color when button is selected.

--- TheSynergyAssessment/RoundedButton.swift
+++ TheSynergyAssessment/RoundedButton.swift
@@ -26,8 +26,22 @@ class RoundedButton: UIButton {
 
 extension RoundedButton {
     
+    private func image(withColor color: UIColor) -> UIImage? {
+        let rect = CGRect(x: 0.0, y: 0.0, width: 30.0, height: 32.0)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        
+        context?.setFillColor(color.cgColor)
+        context?.fill(rect)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+ 
     func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
-        self.setBackgroundImage(image(for: state), for: state)
+        self.setBackgroundImage(image(withColor: color), for: state)
     }
     
     @IBInspectable var defaultColor: UIColor? {
